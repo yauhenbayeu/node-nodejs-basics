b3d13e10e29c8cb3a37b30ec57a3a70925771a6d
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-10-06 22:24:04 +0200

    feat: fs create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from "node:fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "./files/fresh.txt");
+  fs.writeFile(filePath, "I am fresh and young", { flag: "wx" }, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
