1a73b066e881ca8796a91612561f41e76977e080
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-10-06 22:41:34 +0200

    feat: fs copy task

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,10 +2,9 @@ import fs from "node:fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const create = async () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
   const filePath = path.join(__dirname, "./files/fresh.txt");
   fs.writeFile(filePath, "I am fresh and young", { flag: "wx" }, (err) => {
     if (err) {
