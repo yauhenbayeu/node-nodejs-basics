d57bc4b46bd5d82cc97e5cd47e0c8350a2969f5b
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-10-08 00:08:19 +0200

    feat: list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import path from "path";
+import { readdir } from "node:fs/promises";
+import { fileURLToPath } from "url";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const filesFolder = path.join(__dirname, "./files");
+    const files = await readdir(filesFolder, { withFileTypes: true });
+    const namesArr = [];
+
+    for (const file of files) {
+      if (file.isFile()) {
+        namesArr.push(file.name);
+      }
+    }
+    console.log(namesArr);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
