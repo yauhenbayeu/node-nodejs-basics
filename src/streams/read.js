de6d057aa1adb18edba60ab6c5f9d141f9e92f32
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-10-08 01:12:56 +0200

    feat: streams read and write

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const filePath = path.join(__dirname, "./files/fileToRead.txt");
+  const fileStream = fs.createReadStream(filePath);
+
+  fileStream.pipe(process.stdout);
+
+  fileStream.on("error", (err) => {
+    throw new Error("FS operation failed");
+  });
 };
 
 await read();
