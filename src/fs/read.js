6a6244d68efdd1c4cfc06dfa4fec21b8a66320e8
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2024-10-08 00:15:25 +0200

    feat: read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const readFile = path.join(__dirname, "./files/fileToRead.txt");
+
+  fs.access(readFile, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+  });
+
+  const stream = fs.createReadStream(readFile, "utf-8");
+  let readFileData = "";
+  stream.on("data", (chunk) => (readFileData += chunk));
+  stream.on("end", () => console.log(readFileData));
 };
 
 await read();
